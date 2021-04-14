3adf7d728cad41a8f0fca52487fb7bf0f604d365
jiabailong <jiabailong@100tal.com>
2021-04-14 11:46:18 +0800

    token 校验

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.demo.mapper.UerMaper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -56,7 +59,27 @@ public class TestController {
         if(list==null||list.size()==0){
             return"no user";
         }
-        String token=JWT.create().withAudience(user.getName()).sign(Algorithm.HMAC256(user.getPhone()));
+        Algorithm algorithm=Algorithm.HMAC256("109800099877");
+        String token=JWT.create().withAudience("ATI").withClaim("name",name).sign(algorithm);
         return token;
+    }
+    @GetMapping("test")
+    public  String veryToken(@RequestParam String token){
+        Algorithm algorithm=Algorithm.HMAC256("109800099877");
+        DecodedJWT decodedJWT=null;
+        try {
+         decodedJWT = JWT.require(algorithm).build().verify(token);
+
+        }catch (SignatureVerificationException e){
+            e.printStackTrace();
+        }
+        if(decodedJWT==null){
+            return "验证失败";
+        }
+        System.out.println("getHeader=="+decodedJWT.getHeader());
+        System.out.println("getPayload=="+decodedJWT.getPayload());
+        System.out.println("claim=="+decodedJWT.getClaims().get("name"));
+
+        return decodedJWT.getClaims().get("name").asString();
     }
 }
